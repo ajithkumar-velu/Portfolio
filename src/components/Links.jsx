41e17e27ba891ce2bf8683d07c6f12d251163cd5
Ajith Kumar V <ajithajk46@gmail.com>
2025-04-09 11:40:47 +0530

    updated links button

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -5,7 +5,7 @@ import { images, links } from '../assets/assets'
 const Links = () => {
   return (
     <div className='flex gap-2 items-center m-auto xl:m-0' >
-        <Button  btn={<a href={links.github} target='blank' > <img src={images.githubIcon} className='w-8' /></a>} cls="px-1 py-1 bg-btn-unique shadow-[-3px_-1px_3px_#152b2b,2px_1px_1px_#D6BD98] active:translate-x-[0.5%] active:translate-y-[0.5%] active:shadow-[-1px_-0.4px_3px_#152b2b] hover:scale-105" />
+        <Button  btn={<a href={links.github} target='blank' > <img src={images.githubIcon} className='w-7' /></a>} cls="px-[5px] py-[5px] bg-btn-unique shadow-[-3px_-1px_3px_#152b2b,2px_1px_1px_#D6BD98] active:translate-x-[0.5%] active:translate-y-[0.5%] active:shadow-[-1px_-0.4px_3px_#152b2b] hover:scale-105" />
         <Button  btn={<a href={links.linkedIn} target='blank' > <img src={images.linkedInIcon} className='w-7' /></a>} cls="px-[5px] py-[5px] bg-btn-unique shadow-[-3px_-1px_3px_#152b2b,2px_1px_1px_#D6BD98] active:translate-x-[0.5%] active:translate-y-[0.5%] active:shadow-[-1px_-0.4px_3px_#152b2b] hover:scale-105" />
         <Button  btn={<a href={links.leetcode} target='blank' > <img src={images.leetcodeIcon} className='w-7' /></a>} cls="px-[5px] py-[5px] bg-btn-unique shadow-[-3px_-1px_3px_#152b2b,2px_1px_1px_#D6BD98] active:translate-x-[0.5%] active:translate-y-[0.5%] active:shadow-[-1px_-0.4px_3px_#152b2b] hover:scale-105" />
     </div>
